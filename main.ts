db50f57a962a416eabdde69bc9f675f940523c5d
B <bryan.nelson@stu.ocsb.ca>
2025-12-19 12:23:59 -0500

    Changed radio group, changed comments and fixed the readme

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bryan
+ * Created on: Dec 18
+ * This program recieves the message to close when  sonar from another microbit is too close
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(16)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+// receiver code
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
+
+
